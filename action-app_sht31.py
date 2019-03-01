d186ed5412ed02d5b7eb52ece17d818f3481b24f
Bors, Ltd <github@bors-ltd.fr>
2019-03-01 18:16:17 +0100

    Reformatage en Py3 et mon template

--- action-app_sht31.py
+++ action-app_sht31.py
@@ -1,43 +1,42 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import traceback
 
-from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
-import io
+from hermes_python.ontology import MqttOptions
 import time
 import smbus2
 
-CONFIG_INI = "config.ini"
 
-class Temperature_Humidity_SHT31(object):
-    def __init__(self):
+def subscribe_intent_callback(hermes, intent_message):
+    action_wrapper = ActionWrapper(hermes, intent_message)
+    try:
+        action_wrapper.action()
+    except SensorError:
+        action_wrapper.say("Désolée, je n'ai pas de réponse des capteurs.")
+    except Exception:
+        traceback.print_exc()
+        action_wrapper.say("Désolée, il y a eu une erreur.")
 
-        try:
-            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
-        except :
-            self.config = None
-            print "[Error] No config.ini file found! Please check.."
-            sys.exit(1)
 
-        self.mqtt_host = self.config.get('secret',{"mqtt_host":"localhost"}).get('mqtt_host','localhost')
-        self.mqtt_port = self.config.get('secret',{"mqtt_port":"1883"}).get('mqtt_port','1883')
-        self.mqtt_addr = "{}:{}".format(self.mqtt_host, self.mqtt_port)
+class SensorError(Exception):
+    pass
 
-        self.site_id = self.config.get('secret',{"site_id":"default"}).get('site_id','default')
-        self.if_fahrenheit = self.config.get('secret',{"if_fahrenheit":"false"}).get('if_fahrenheit', "false")
+
+class ActionWrapper:
+    def __init__(self, hermes, intent_message):
+        self.hermes = hermes
+        self.intent_message = intent_message
+        self.site_id = 'default'  # TODO
         self.bus = smbus2.SMBus(1)
-        self.start_blocking()
 
-    def get_temperature_humidity(self, ret = 'cTemp'):
-
+    def get_temperature_humidity(self, ret='temperature'):
         try:
             self.bus.write_i2c_block_data(0x44, 0x2C, [0x06])
             time.sleep(0.2)
             data = self.bus.read_i2c_block_data(0x44, 0x00, 6)
-        except IOError:
-            print "[Error] No sensor found"
-            return
+        except IOError as exc:
+            print("[Error] No sensor found")
+            raise SensorError from exc
 
         temp = data[0] * 256 + data[1]
         cTemp = -45 + (175 * temp / 65535.0)
@@ -48,44 +47,35 @@ class Temperature_Humidity_SHT31(object):
         if ret == 'humidity':
             return humidity
 
-    def c_to_f(self, c):
-        return c * 9.0 / 5.0 + 32.0
-
-    def askTemperature(self, hermes, intent_message):
+    def askTemperature(self):
         temp = round(self.get_temperature_humidity('temperature'), 1)
-        temp_f = round(self.c_to_f(temp), 1)
+        print("Celsius:", temp, "°C")
 
-        print "Celsius: {}*C / Fahrenheit {}*F".format(temp, temp_f)
-        msg = "The current temperature is {}{}"
+        self.say("Il fait actuellement", temp, "degrés")
 
-        if self.if_fahrenheit.lower() == "false":
-            msg = msg.format(temp, ' degree')
-        elif self.if_fahrenheit.lower() == "true":
-            msg = msg.format(temp_f, ' fahrenheit degree')
-        else:
-            msg = "No unit specified, please check config.ini file"
-
-        hermes.publish_end_session(intent_message.session_id, msg)
-
-    def askHumidity(self, hermes, intent_message):
+    def askHumidity(self):
         humidity = round(self.get_temperature_humidity('humidity'), 1)
+        print("Humidity:", humidity, "%")
 
-        print "Humidity: {}%".format(humidity)
-        msg = "The current humidity is {}%".format(humidity)
+        self.say("L'humidité est de", humidity, "%")
 
-        hermes.publish_end_session(intent_message.session_id, msg)
-
-    def master_intent_callback(self,hermes, intent_message):
-        if intent_message.site_id != self.site_id:
+    def action(self):
+        if self.intent_message.site_id != self.site_id:
             return
-        if intent_message.intent.intent_name == 'checkTemperature':
-            self.askTemperature(hermes, intent_message)
-        if intent_message.intent.intent_name == 'checkHumidity':
-            self.askHumidity(hermes, intent_message)
+        if self.intent_message.intent.intent_name == 'checkTemperature':
+            self.askTemperature()
+        if self.intent_message.intent.intent_name == 'checkHumidity':
+            self.askHumidity()
 
-    def start_blocking(self):
-        with Hermes(self.mqtt_addr) as h:
-            h.subscribe_intents(self.master_intent_callback).start()
+    def say(self, message, *args):
+        current_session_id = self.intent_message.session_id
+        message = message + " ".join(args)
+        self.hermes.publish_end_session(current_session_id, message)
+
 
 if __name__ == "__main__":
-    Temperature_Humidity_SHT31()
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent('borsltd:checkTemperature', subscribe_intent_callback)
+        h.subscribe_intent('borsltd:checkHumidity', subscribe_intent_callback)
+        h.start()
